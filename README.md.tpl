ea5a8b11fd0918b4501dba6051b3cda497745b44
Petko D. Petkov <pdp@websecurify.com>
2021-05-05 23:34:21 +0000

    Simple whois transform.

---
+++ README.md.tpl
@@ -0,0 +1,61 @@
+[![Follow on Twitter](https://img.shields.io/twitter/follow/pownjs.svg?logo=twitter)](https://twitter.com/pownjs)
+[![NPM](https://img.shields.io/npm/v/@pown/whois.svg)](https://www.npmjs.com/package/@pown/whois)
+[![Fury](https://img.shields.io/badge/version-2x%20Fury-red.svg)](https://github.com/pownjs/lobby)
+![default workflow](https://github.com/pownjs/nucleoid/actions/workflows/default.yaml/badge.svg)
+[![SecApps](https://img.shields.io/badge/credits-SecApps-black.svg)](https://secapps.com)
+
+# Pown Whois
+
+Library, utility and transform for whois operations.
+
+## Credits
+
+This tool is part of [secapps.com](https://secapps.com) open-source initiative.
+
+```
+  ___ ___ ___   _   ___ ___  ___
+ / __| __/ __| /_\ | _ \ _ \/ __|
+ \__ \ _| (__ / _ \|  _/  _/\__ \
+ |___/___\___/_/ \_\_| |_|  |___/
+  https://secapps.com
+```
+
+## Quickstart
+
+This tool is meant to be used as part of [Pown.js](https://github.com/pownjs/pown) but it can be invoked separately as an independent tool.
+
+Install Pown first as usual:
+
+```sh
+$ npm install -g pown@latest
+```
+
+Invoke directly from Pown:
+
+```sh
+$ pown whois
+```
+
+Otherwise, install this module locally from the root of your project:
+
+```sh
+$ npm install @pown/whois --save
+```
+
+Once done, invoke pown cli:
+
+```sh
+$ POWN_ROOT=. ./node_modules/.bin/pown-cli whois
+```
+
+You can also use the global pown to invoke the tool locally:
+
+```sh
+$ POWN_ROOT=. pown whois
+```
+
+## Usage
+
+```
+{{usage}}
+```
---
+++ transforms/whois/index.js
@@ -0,0 +1,115 @@
+const ipRangeCheck = require('ip-range-check')
+const { makeId } = require('@pown/recon/lib/utils')
+const { isIpv4 } = require('@pown/recon/lib/detect')
+const { Transform } = require('@pown/recon/lib/transform')
+const { IPV4_TYPE, IPV6_TYPE, DOMAIN_TYPE } = require('@pown/recon/lib/types')
+
+const { whois } = require('../../lib/whois')
+
+const WHOIS_REPORT_TYPE = 'whois:report'
+
+const cache = {}
+
+const whoisReport = class extends Transform {
+    static get alias() {
+        return ['whois_report', 'whois']
+    }
+
+    static get title() {
+        return 'Whois Report'
+    }
+
+    static get description() {
+        return 'Extract whois information.'
+    }
+
+    static get group() {
+        return this.title
+    }
+
+    static get tags() {
+        return ['ce']
+    }
+
+    static get types() {
+        return [IPV4_TYPE, IPV6_TYPE, DOMAIN_TYPE]
+    }
+
+    static get options() {
+        return {
+            server: {
+                type: 'string',
+                describe: 'Server to query',
+                default: ''
+            },
+
+            follow: {
+                type: 'number',
+                describe: 'Number of redirects to follow',
+                default: 3
+            },
+
+            fastLookup: {
+                type: 'boolean',
+                describe: 'Perform as little whois lookups as possible',
+                default: false
+            },
+
+            extractAsn: {
+                type: 'boolean',
+                describe: 'Extract asn nodes',
+                default: false
+            }
+        }
+    }
+
+    static get priority() {
+        return 1
+    }
+
+    static get noise() {
+        return 5
+    }
+
+    async handle({ id: source = '', label = '' }, options) {
+        const { fastLookup, extractAsn, ...whoisOptions } = options || {}
+
+        const results = []
+
+        let report
+
+        if (fastLookup && isIpv4(label)) {
+            for (let [cidr, cachedReport] of Object.entries(cache)) {
+                if (cidr.split(/[\s,]+/).map(c => c.trim()).filter(c => c).some((c => ipRangeCheck(label, c)))) {
+                    report = cachedReport
+                }
+            }
+        }
+
+        if (!report) {
+            report = await whois(label, whoisOptions)
+        }
+
+        const id = makeId(WHOIS_REPORT_TYPE, label)
+
+        results.push({ id, type: WHOIS_REPORT_TYPE, label, props: { ...report }, edges: [source] })
+
+        if (extractAsn) {
+            const { origin, originAs, asn = origin || originAs, organization } = report
+
+            if (asn) {
+                results.push({ type: 'asn', lavbel: asn, props: { asn, organization }, edges: [source, id] })
+            }
+        }
+
+        const { cidr } = report
+
+        if (cidr) {
+            cache[cidr] = report
+        }
+
+        return results
+    }
+}
+
+module.exports = { whoisReport }
